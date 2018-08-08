80dff8a6b6210331f1674f01e733176e15efb553
DooMachine <okanaslankan@hotmail.com>
2018-08-08 20:41:14 +0300

    fix migrations

--- src/ApiServices/MicroStarter.Api/migrations.dev.sh
+++ src/ApiServices/MicroStarter.Api/migrations.dev.sh
@@ -2,6 +2,6 @@
 cd src
 rm -rf Migrations
 
-dotnet ef migrations add InitialMigrations -c MicroStarterApiContext -o Migrations/IdentityServer/UsersDb
+dotnet ef migrations add InitialMigrations -c MicroStarterApiContext -o Migrations/MicroStarter.Api/UsersDb
 dotnet ef migrations script -c MicroStarterApiContext -o Migrations/MicroStarter.Api/MicroStarterApiContext.sql
 cd ..
